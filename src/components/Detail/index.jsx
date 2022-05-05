14a7a2037fc94623b3ae5b0344b835c1869c232a
Luděk Roleček <ludek@rolecek.cz>
2022-05-05 19:18:51 +0200

    Aktualizace

--- src/components/Detail/index.jsx
+++ src/components/Detail/index.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './style.css';
-
+import {Link} from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 
 
@@ -8,10 +8,29 @@ const Detail = () => {
 
 	const {id} = useParams();
 
+	const [zbozi, setZbozi] = useState({});
+
+	useEffect(
+		() => {
+			fetch(`https://swapi.dev/api/people/${id}`)
+			.then(response => response.json())
+			.then(data => {
+				setZbozi(data);
+			});
+		},
+		[id]
+	)
+
+
+
 	return (
 		<div className="komponenta">
 			<h3>Detail zboží: {id}</h3>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex, dolor!</p>
+			<p>Nazev: {zbozi.name}</p>
+			<p>Barva: {zbozi.eye_color}</p>
+			<p>Záruka do: {zbozi.birth_year}</p>
+			<hr />
+			<Link to="/produkty">Zpět na přehled produktů</Link>
 		</div>
 	)
 }
--- src/components/Produkty/index.jsx
+++ src/components/Produkty/index.jsx
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './style.css';
 
+import {Link} from 'react-router-dom';
+
 const Produkty = () => {
+
+	const [seznam, setSeznam] = useState([]);
+
+	useEffect(
+		() => {
+			fetch(`https://swapi.dev/api/people`)
+			.then(response => response.json())
+			.then(data => {
+				setSeznam(data.results);
+			});
+		},
+		[]
+	)
+
+
 
 	return (
 		<div className="komponenta">
 			<h3>Produkty</h3>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex, dolor!</p>
+
+			<ul>
+				{/* <li><Link to="/produkty/1">Luke</Link></li>
+				<li><Link to="/produkty/2">C3PO</Link></li>
+				<li><Link to="/produkty/3">R2D2</Link></li>
+				<li><Link to="/produkty/4">Taťka Darth Vader</Link></li> */}
+
+				{ seznam.map((person, index) => <li><Link to={`/produkty/${index+1}`}>{person.name}</Link></li> ) }
+			</ul>
+
 		</div>
 	)
 }
