b6524dd0496ce58718e25e0403a6431ebfb025fd
Luděk Roleček <ludek@rolecek.cz>
2022-05-05 18:51:48 +0200

    Aktualizace

--- src/components/Detail/index.jsx
+++ src/components/Detail/index.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import './style.css';
 
+import { useParams } from 'react-router-dom';
+
+
 const Detail = () => {
+
+	const {id} = useParams();
 
 	return (
 		<div className="komponenta">
-			<h3>Detail zboží</h3>
+			<h3>Detail zboží: {id}</h3>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex, dolor!</p>
 		</div>
 	)
